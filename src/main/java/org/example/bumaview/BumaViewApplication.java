a6b63507951956c79f66405b5340809bf2d6c420
sunz8281 <sunz8281@gmail.com>
2025-09-17 13:27:22 +0900

    기본 세팅

---
+++ src/main/java/org/example/bumaview/BumaViewApplication.java
@@ -0,0 +1,12 @@
+package org.example.bumaview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BumaViewApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BumaViewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bumaview/BumaViewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bumaview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BumaViewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
